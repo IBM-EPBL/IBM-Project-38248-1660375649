2e34cef3edd9a1e783edb634217c544bc540de89
620619104003 <114217546+620619104003@users.noreply.github.com>
2022-11-15 15:19:53 +0530

    Update and rename app.py to IBM...app.py

--- Project Development Phase/Sprint 4/IBM...app.py
+++ Project Development Phase/Sprint 4/IBM...app.py
@@ -3,8 +3,17 @@ import pandas as pd
 from flask import Flask, request, render_template
 import pickle
 
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "J69b3hO8Xbf8BNeV5qJ73IszoLGx0S-_MVmEVd3fywrG"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
 app = Flask(__name__)
-model = pickle.load(open('CKD.pkl', 'rb'))
+#model = pickle.load(open('CKD.pkl', 'rb'))
 
 
 @app.route('/')
@@ -25,8 +34,8 @@ def my_home():
 @app.route('/predict', methods=['POST'])
 def predict():
     #input_features = ([int(x) for x in request.form.values()])
-    blood_urea = request.form["blood_urea"]
-    blood_glucose_random = request.form["blood_glucose_random"]
+    blood_urea = int(request.form["blood_urea"])
+    blood_glucose_random = int(request.form["blood_glucose_random"])
     anemia = request.form["Anemia"]
     if (anemia == "no"):
         anemia = 0
@@ -67,12 +76,20 @@ def predict():
     print(input_features)
     features_value = [np.array(input_features)]
 
+    payload_scoring = {"input_data": [{"field": [['blood_urea','blood_glucose_random','anemia','coronary_artery_disease','pus_cell','red_blood_cell','diabetics_mellitus','pedal_edema']], "values":features_value}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/f887afa8-65fa-40e0-849b-5b180653e583/predictions?version=2022-11-02', json=payload_scoring,headers={'Authorization': 'Bearer ' + mltoken})
+    print(response_scoring)
+    predictions=response_scoring.json()
+    predict=predictions['predictions'][0]['values'][0][0]
+    print("Final prediction :",predict)
+   
 
     #features_name = ['red_blood_cells','pus_cell','blood glucose random','blood_urea','pedal_edema','anemia','diabetesmellitus','coronary_artery_disease']
     features_name = ['blood_urea','blood glucose random','anemia','coronary_artery_disease','pus_cell','red_blood_cells','diabetesmellitus','pedal_edema' ]
     df = pd.DataFrame(features_value, columns=features_name)
-    output = model.predict(df)
-    return render_template('result.html', prediction_text=output)
+    #output = model.predict(df)
+    return render_template('result.html', prediction_text=predict)
 
 
 # Press the green button in the gutter to run the script.
